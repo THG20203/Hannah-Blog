9819dca0312011f0587264c756ac87169db0d032
TristanG <tristanhgriffiths8@yahoo.com>
2021-03-17 15:02:20 +0000

    march seventeenth

--- app.js
+++ app.js
@@ -12,7 +12,9 @@ app.set('view engine', 'ejs');
 //                 SETTING THE FILE PATHS FOR CSS/IMAGES
 // Set the folder for css
 app.use(express.static(path.join(__dirname, 'css')));
-// Set the folder for images
+// Set the folder for blog background images
+app.use(express.static(path.join(__dirname, 'img')));
+// Set the folder for general images
 app.use(express.static(path.join(__dirname, 'images')));
 //Set the folder for Node Modules
 app.use(express.static(path.join(__dirname, 'node_modules')));
@@ -25,13 +27,31 @@ app.get('/', function(req, res) {
   res.render('index');
 });
 
-app.get('/portal', function(req, res) {
-  res.render('portal');
+app.get('/about', function(req, res) {
+  res.render('about');
 });
 
-app.get('/reasons', function(req, res) {
-  res.render('reasons');
+app.get('/meet', function(req, res) {
+  res.render('meet');
 });
+
+app.get('/blog', function(req, res) {
+  res.render('blog');
+});
+
+app.get('/lifegets', function(req, res) {
+  res.render('lifegets');
+});
+
+app.get('/gallery', function(req, res) {
+  res.render('gallery');
+});
+
+app.get('/press', function(req, res) {
+  res.render('press');
+});
+
+
 
 //Tune into port for Heroku
 let port = process.env.PORT;
