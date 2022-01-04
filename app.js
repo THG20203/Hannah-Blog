c8f8e3fa633efa29bc4ef4f5f43afa2df9c25e2b
TristanG <tristanhgriffiths8@yahoo.com>
2022-01-04 01:27:52 +0000

    3rd Jan

--- app.js
+++ app.js
@@ -59,5 +59,5 @@ if (port == null || port == "") {
 
 // Tune into port 3000
 app.listen(port, function() {
-  console.log("Server has started succesfully");
+  console.log("Server started succesfully");
 });
